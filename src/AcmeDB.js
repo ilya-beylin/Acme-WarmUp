8119bade49703f89a3d0f7e418a12b7df7798f0e
Eric Katz <ericpkatz@gmail.com>
2017-01-21 10:55:53 -0500

    Prof comments
    good work!

--- src/AcmeDB.js
+++ src/AcmeDB.js
@@ -1,18 +1,21 @@
 class AcmeDB{
 
-constructor(obj){
-    this.index = 1;
-    this.users = [];
+constructor(obj){//call this users
+    this.index = 1;//i don't think you need this
+    this.users = [];//loop over all passed in users and call addUser
     this.addUser(obj);
   }
 
- addUser(obj){
+ addUser(obj){//call this user
+    //use reduce to find the maxI
     obj.id = this.index;
     this.users.push(obj);
     this.index++;
   }
 
   showUsers(){
+    //use map and join instead of concatenation
+    //also in es6 you can use backticks instead of concatenation
     let str = "";
     for(var i = 0; i<this.users.length; i++){
       str += this.users[i].name + " ";
@@ -21,7 +24,9 @@ constructor(obj){
   }
 
   findById(id){
-    for(var i = 0; i<this.users.length; i++){
+    //give operators some space
+    //you could also use filter here.
+    for(var i = 0; i < this.users.length; i++){
       if(this.users[i].id == id){
         return this.users[i];
       }
@@ -29,6 +34,8 @@ constructor(obj){
   }
 
   removeUserById(id){
+    //too complicated.. use filter
+    //also a for loop within a for loop is probably not needed
     for(var i = 0; i< this.users.length; i++){
       if(this.users[i].id == id){
         for(var j=i; j < (this.users.length - 1); j++){
@@ -39,9 +46,9 @@ constructor(obj){
     }
   }
 
-  editUser(obj){
-    var user = this.findById(obj.id);
-    user.name = obj.name;
+  editUser(obj){//call this user
+    var user = this.findById(obj.id);//i like this!
+    user.name = obj.name;//what if you obj had other edited properties...
   }
 }
 
